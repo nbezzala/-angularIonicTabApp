1930da4ee974b05f4f5d2b923275b583c8611b41
nbezzala <nbezzala@yahoo.com>
2022-02-18 16:20:42 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-k5zpwd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-k5zpwd)
---
+++ src/app/tab1/fortune-cookie/fortune-cookie.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-fortune-cookie',
+  templateUrl: './fortune-cookie.component.html',
+  styleUrls: ['./fortune-cookie.component.css'],
+})
+export class FortuneCookieComponent implements OnInit {
+  constructor(public loadingController: LoadingController) {}
+
+  ngOnInit() {}
+
+  async showFortune() {
+    const loading = await this.loadingController.create({
+      duration: 3000,
+      message: 'please wait',
+      spinner: 'dots',
+    });
+    await loading.present();
+
+    const {role, data} = await loading.onDidDismiss();
+    alert('Your fortune is sweet ' + role + ' ' + data);
+  }
+}
